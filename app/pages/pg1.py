41a85e5f2729ddc70935cc79ef884f11d9863db6
shane sarabdial <satramsarabdial12@gmail.com>
2023-05-19 13:39:59 -0400

    updated app and model

--- app/pages/pg1.py
+++ app/pages/pg1.py
@@ -3,6 +3,7 @@ from dash import dcc, html, callback, Output, Input
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import pandas as pd
+from PIL import Image
 
 # To create meta tag for each page, define the title, image, and description.
 dash.register_page(__name__,
@@ -14,71 +15,80 @@ dash.register_page(__name__,
                    )
 
 # page 1 data
-geo = pd.read_csv(R'C:\Users\shane\Desktop\Dev\Capstone\WeatherWatts-\app\Data\geo3.csv')
+iso = pd.read_csv('./Data/iso.csv')
+geo = pd.read_csv('./Data/geo3.csv')
 
-geo_year = geo.query('Year == 2015')
-fig_geo = px.scatter_mapbox(geo_year, lat='Lat', lon='Long', title="Temperature by City From 2015 to 2023",
-                            center=dict(lat=35.09024, lon=-95.712891),
-                            zoom=2.7, mapbox_style="open-street-map", color_continuous_midpoint=50, opacity=.5,
-                            color_continuous_scale='sunsetdark', size='Dew', color='Temperature',
-                            text='City', labels='City',size_max=25)
+fig_iso = px.choropleth(iso, locations="State",
+                        color="ISO",  # lifeExp is a column of gapminder
+                        hover_name="ISO",  # column to add to hover information
+                        color_continuous_scale=px.colors.sequential.Plasma,
+                        locationmode='USA-states',
+                        scope='usa',
+                        labels={'State': 'State'},
+                        color_discrete_sequence=px.colors.qualitative.Pastel,
 
+                        )
+fig_iso.add_scattergeo(
+    locations=iso['State'],
+    locationmode='USA-states',
+    text=iso['State'],
+    mode='text',
+    showlegend=False,
+    hoverinfo='none',
+)
+fig_geo = px.scatter_geo(geo, lon='Long', lat='Lat',
+                         hover_data=['Year', 'Temperature', 'Dew', 'Wind Speed', 'Cloud Cover',
+                                     'UV Index'], scope='usa', hover_name='City',
+                         color_discrete_sequence=['rgb(10, 10, 10)'], size='Temperature', size_max=7)
 
-col_options = [
-            'Temperature','Dew', 'Humidity', 'Percipitation Probability', 'Snow', 'Wind Speed',
-            'Sea Level Pressure', 'Cloud Cover','Solar Radiation', 'UV Index',]
+fig_geo.add_scattergeo(
+    lon=geo['Long'],
+    lat=geo['Lat'], marker_size=8, marker_color='rgb(10, 10, 10)', showlegend=False,
+)
+fig_iso.update_layout(
+    title={'text': 'ISO Regions',
+           'xanchor': 'center',
+           'yanchor': 'top',
+           'x': 0.5,
+           'font_size': 40, })
+fig_iso.add_trace(fig_geo.data[0])
 layout = html.Div(
     [
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        html.H4('Write summary')
-                    ], xs=10, sm=10, md=8, lg=4, xl=4, xxl=4
+                        dcc.Markdown('''In this project we aim to predict and identify key features that can impact
+                         energy demand in the United States. We used weather and a energy ETF.
+                         [XLE](https://finance.yahoo.com/quote/XLF?p=XLF&.tsrc=fin-srch) is a index that tracks the
+                         performance of in the energy industry. Our focus are on the states of California, New York,
+                         Texas and Florida. The weather data was taken from a major city in each state. They are 
+                         Austin, Texas, New York City, NY, Tampa, Florida and Los Angeles, California. In the US there
+                         are non-profit organizations that facilitate fair competition and ensure non-discriminatory
+                         access to energy, they are called Independent System Operators 
+                         [ISO](https://en.wikipedia.org/wiki/Regional_transmission_organization_(North_America)). With
+                         the exception of Florida the other 3 states are ISOs. To get the full details of our mythology
+                         in constructing our model vist our [github](https://github.com/shane-sarabdial/WeatherWatts-).
+                         ''', style={'textAlign': 'center'}
+                                     )
+                    ], xs=12, sm=12, md=11, lg=8, xl=8, xxl=8
                 )
-            ]
+            ], justify='around'
         ),
+        html.Br(),
+        html.Br(),
         html.Br(),
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        dcc.Dropdown(id='year', options=[{'label': x, 'value': x} for x in range(2015, 2024)],
-                                     placeholder='Year', value=2015, maxHeight=150)
-                    ], width={'size': 3, 'offset': 2}
-                ),
-                dbc.Col(
-                    [
-                        dcc.Dropdown(id='col', options=[{'label': x, 'value': x} for x in col_options],
-                                     placeholder='Temperature', maxHeight=150, value='Temperature')
-                    ], width={'size': 3, 'offset': 0}
-                )
-
-            ]
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
                         dcc.Graph(id='map',
-                                  figure=fig_geo, style={'width': '82vw', 'height': '60vh'}
+                                  figure=fig_iso, style={'width': '60vw', 'height': '60vh'}
 
                                   )
-                    ], width={'size': 10, 'offset': 1}
+                    ], xs=12, sm=12, md=8, lg=8, xl=8, xxl=8
                 )
-            ]
+            ], justify='around'
         )
     ]
 )
-@callback(
-    Output('map', 'figure'),
-    Input('year', 'value'),
-    Input('col', 'value')
-)
-def update_map(year, col):
-    geo_year_1 = geo.query(f'Year == {year}')
-    fig_geo = px.bar(geo_year_1, x='City', y=col)
-    print(year)
-    print(col)
-    return fig_geo
-
